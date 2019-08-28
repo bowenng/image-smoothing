8ac0d9a9cb921f4859a4c08920fb8a106ee0e888
bowenng <bowen-wu@hotmail.com>
2019-08-28 11:26:35 -0400

    update makeedge: keep output same size

---
+++ core/data/makeedge.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+from core.data.utils import is_image
+import os
+
+
+def make_edge_files(image_dir, edge_dir, cwd):
+    os.chdir(cwd)
+    os.mkdir(edge_dir)
+    image_files = os.listdir(image_dir)
+    images = filter(is_image, image_files)
+
+    for image_name in images:
+        image = cv.imread(os.path.join(image_dir, image_name), 0)
+        edges = cv.Canny(image, 100, 200)
+        cv.imwrite(os.path.join(edge_dir, "edge_"+image_name), edges)
+
+    return edges
+
+
