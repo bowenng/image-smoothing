bdb447be63fa580cc9d126dbde98021bd07559fc
bowenng <bowen-wu@hotmail.com>
2019-08-28 11:21:55 -0400

    update edgeresponse

--- core/loss/edgeresponse.py
+++ core/loss/edgeresponse.py
@@ -1,27 +1,46 @@
 import torch
 from torch import nn
+from torch.functional import F
 
 
 class EdgeResponse(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, image):
-        # cut the border of the image
-        image_without_border = image[:, :, 1:-1, 1:-1]
-
-        # get metadata needed to calculate edge response
-        image_shape = image.shape
-        width = image_shape[2]
-        height = image_shape[3]
-
-        # calculate the edge response in a 3x3 window
-        adjacent_differences = []
-        # loop through every pixel j around the pixel i, and calculate the absolute difference
-        for x in range(-1, 2):
-            for y in range(-1, 2):
-                adjacent_differences.append(torch.abs(torch.sum(image[:, :, 1+x:width-1+x, 1+y:height-1+y]-image_without_border, dim=1)))
-
-        # sum over pixel j
-        edge_response = torch.stack(adjacent_differences, dim=0).sum(dim=0).unsqueeze(1)
+    def forward(self, images):
+        edge_response = self.edge_response_kernel(images)
         return edge_response
+
+    def edge_response_kernel(self, images):
+        """
+        1. reflection pads the images
+        2. use the kernel below to calculate edge response
+
+        kernel used to calculate edge response:
+            [
+                [-1, -1, -1],
+                [-1, 8, -1],
+                [-1, -1, -1]
+            ]
+        :return: Tensor of the same shape of image
+        """
+
+        # reflection pads the images
+        reflection_pad = nn.ReflectionPad2d(2)
+        images = reflection_pad(images)
+
+        # use the kernel below to calculate edge response
+        n_channels = images.shape[1]
+
+        kernel_weights = torch.Tensor([
+            [-1, -1, -1],
+            [-1, 8, -1],
+            [-1, -1, -1]
+        ])
+
+        # repeat the weights to match
+        kernel_weights = kernel_weights.view(1, 1, 3, 3).repeat(1, n_channels, 1, 1)
+
+        output = F.conv2d(images, kernel_weights)
+
+        return output
